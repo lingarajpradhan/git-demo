2010f61e08d8cbf77c73ec0c09b3a1a2321d0a82
lingarajpradhan <49097849+lingarajpradhan@users.noreply.github.com>
2020-03-06 01:06:25 +0530

    method adde by dev2

--- src/main/java/com/lp/app/git_demo/App.java
+++ src/main/java/com/lp/app/git_demo/App.java
@@ -8,6 +8,10 @@ public class App
 			System.out.println("welcome");
 		}
 	}
+	
+	public void addedMethod(){
+		System.out.println("new Method");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
